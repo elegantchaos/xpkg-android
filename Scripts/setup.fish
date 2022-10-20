122f51a76fa70a632aed727e7bb689b9e602d5cb
Sam Deane <sam@elegantchaos.com>
2022-10-20 17:05:22 +0100

    Set android paths

--- Scripts/setup.fish
+++ Scripts/setup.fish
@@ -1,1 +1,6 @@
 # fish.config setup here
+
+set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
+fish_add_path "$ANDROID_HOME/tools"
+fish_add_path "$ANDROID_HOME/tools/bin"
+fish_add_path "$ANDROID_HOME/platform-tools"
